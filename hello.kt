7eaaf08f5a0178fea7f7a29359bb9c745a25641b
Misael Sandoval <msandoval131@outlook.com>
2024-02-22 14:19:27 -0700

    basics of kotlin

--- hello.kt
+++ hello.kt
@@ -1,3 +1,20 @@
 fun main(){
+    //print a string
     println("Hello world!")
+
+    //print numbers
+    print(15*4)
+    println()
+
+    //declare a variable of type string
+    var myString: String = "Random String"
+    println(myString)
+
+    //declaring val means this value is immutable, cannot be changed.
+    val pi: Double = 3.14
+    println(pi)
+
+    //the compiler can make an inference on type
+    var typeTest = 2.6
+    print("${typeTest::class.simpleName}")
 }
